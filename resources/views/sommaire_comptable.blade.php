949cc9b0288ba4cb62b70b30bd3a31126988d06e
MyriamSiad <myriam.siad.sio@gmail.com>
2024-10-24 19:25:21 +0200

    Réalisation : Suivre le paiement fiche de frais
    Suivie du paiement :
    - Page accessible uniquement que pour les comptable connecté, et non les visiteurs
    - Tableau restituant toutes les fiches Frais, en fonction de leurs état  (VA), par Nom Visiteur et le Mois
    
    - Formulaire permettant de valider, par une case à coché, les fiches frais qui ont été remboursé
    
    - Apparition d'un tableau regroupant les fiches qui on été remboursé récemment (Le jours même) , en bas

--- resources/views/sommaire_comptable.blade.php
+++ resources/views/sommaire_comptable.blade.php
@@ -16,11 +16,9 @@
 
                    </li>
                   <li class="smenu">
-                     <a href="{{ route('chemin_gestionFrais')}}" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+                     <a href="{{route('cheminVisiteur')}}" title="Affiche Nom Visiteur ">Suivie fiche Frais</a>
                   </li>
-                  <li class="smenu">
-                    <a href="{{ route('chemin_selectionMois') }}" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
-                  </li>
+                
                <li class="smenu">
                 <a href="{{ route('chemin_deconnexion') }}" title="Se déconnecter">Déconnexion</a>
                   </li>
